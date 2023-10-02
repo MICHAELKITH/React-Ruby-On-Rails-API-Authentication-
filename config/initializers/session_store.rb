55822f41de581dd50c6c01c7765954e8a2c00d69
MICHAELKITH <michaelkithinji27@gmail.com>
2023-10-02 18:31:14 +0300

    Checking domain login

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
+if Rails.env =="production"
 Rails.application.config.session_store :cookie_store, key: "_authetication_app", domain: "http://localhost/3000"
+else
+    Rails.application.config.session_store :cookie_store, key: "_authetication_app"
+end
