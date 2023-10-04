a77b60296b158d84e5fec0135274f99382a6cd7b
MICHAELKITH <michaelkithinji27@gmail.com>
2023-10-04 21:31:58 +0300

    Change the routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :sessions, only [:create]
-  delete :logout to: "sessions#logout"
-  get :logged_in to: "sessions#logged_in"
-  resources :registrations ,only: [:create]
+  resources :sessions, only: [:create]
+  delete :logout, to: "sessions#logout"
+  get :logged_in, to: "sessions#logged_in"
   root to: "static#home"
 end
+
